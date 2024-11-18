61ab443ee5b8e4714053aecc201a43a30d1e209d
Tomk2d <uijinshin01@gmail.com>
2024-11-18 16:59:57 +0900

    update 9095

---
+++ src/DP/b9095.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.util.*;
+import java.io.*;
+
+public class b9095 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(dp[num]);
+        }
+    }
+}
