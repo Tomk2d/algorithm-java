19f9bd7e06e34cb9326993891d8af63d5b4ff357
Tomk2d <uijinshin01@gmail.com>
2024-11-18 15:52:08 +0900

    update 11726

---
+++ src/DP/b11726.java
@@ -0,0 +1,19 @@
+package DP;
+
+import java.util.*;
+import java.io.*;
+
+public class b11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
