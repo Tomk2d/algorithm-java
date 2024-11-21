744f46b608a068964abe09d20284299aa711abb8
Tomk2d <uijinshin01@gmail.com>
2024-11-21 16:43:22 +0900

    update 11727

--- src/DP다시풀기/b11727.java
+++ src/DP다시풀기/b11727.java
@@ -3,19 +3,17 @@ package DP다시풀기;
 import java.util.*;
 import java.io.*;
 
-public class b11726 {
+public class b11727 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+1];
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
+        dp[2] = 3;
 
-        for (int i =4; i<= N; i++){
-            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        for(int i=3;i<=N;i++){
+            dp[i] = ((dp[i-2]*2) + dp[i-1]) %10007;
         }
         System.out.println(dp[N]);
     }
