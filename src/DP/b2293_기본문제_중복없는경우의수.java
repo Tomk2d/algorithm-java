37c859e618344cfd4911a947942c9676bf146c80
Tomk2d <uijinshin01@gmail.com>
2025-04-17 14:16:26 +0900

    update 2293

---
+++ src/DP/b2293_기본문제_중복없는경우의수.java
@@ -0,0 +1,34 @@
+package DP;
+
+import java.io.*;
+import java.util.*;
+
+public class b2293_기본문제_중복없는경우의수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+
+        // 여기서 coin 을 먼저 돌게 하고,
+        // 그 코인을 시작점으로 하여 이후 애들만 돌기때문에
+        // 중복이 생기지 않는다.
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
+
